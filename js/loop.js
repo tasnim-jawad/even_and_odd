fcd9b1dd94cbefcd66265b411b776f793dd02659
tasnim-jawad <muhammadtasnimulhasan1@gmail.com>
2021-08-05 07:10:48 +0600

    ok

--- js/loop.js
+++ js/loop.js
@@ -2,21 +2,33 @@
 
 function while_even(index,range){
     while(index<=range){                    //for even number
-        if(index%2==0)
+       
+        if(index%2==0){
         console.log(index);
+        
         var node = document.createElement("LI");                 // Create a <li> node
         var textnode = document.createTextNode(index);         // Create a text node
         node.appendChild(textnode);                              // Append the text to <li>
         document.getElementById("even_number").appendChild(node);     // Append <li> to <ul> with id="myList"
+
+        }
         index++;
-    }    
+    }
 }
 
 
 function while_odd(index,range){
     while(index<=range){                 //for odd number
-        if(index%2 != 0)
+        if(index%2 != 0){
         console.log(index);
+        
+        
+        var node = document.createElement("LI");                 // Create a <li> node
+        var textnode = document.createTextNode(index);         // Create a text node
+        node.appendChild(textnode);                              // Append the text to <li>
+        document.getElementById("odd_number").appendChild(node);     // Append <li> to <ul> with id="myList"
+        
+        }
         index++;
     }
 }
@@ -26,8 +38,15 @@ function while_odd(index,range){
 
 function do_while_even(index,range){
     do{                                 //for even number
-        if(index%2 == 0)
+        if(index%2 == 0){
         console.log(index);
+
+        var node = document.createElement("LI");                 // Create a <li> node
+        var textnode = document.createTextNode(index);         // Create a text node
+        node.appendChild(textnode);                              // Append the text to <li>
+        document.getElementById("even_number").appendChild(node);     // Append <li> to <ul> with id="myList"
+
+        }
         index++;
     }while(index<=range);
 }
@@ -35,8 +54,15 @@ function do_while_even(index,range){
 
 function do_while_odd(index,range){
     do{                                  //for odd number
-        if(index%2 != 0)
+        if(index%2 != 0){
         console.log(index);
+         
+        var node = document.createElement("LI");                 // Create a <li> node
+        var textnode = document.createTextNode(index);         // Create a text node
+        node.appendChild(textnode);                              // Append the text to <li>
+        document.getElementById("odd_number").appendChild(node);     // Append <li> to <ul> with id="myList"
+        
+        }
         index++;
     }while(index<=range)
 }
@@ -48,15 +74,29 @@ function do_while_odd(index,range){
  
 function for_evens(index,range){
     for(index;index<=range;index++){                   //for even number
-        if(index%2 == 0)
+        if(index%2 == 0){
         console.log(index);
+
+        var node = document.createElement("LI");                 // Create a <li> node
+        var textnode = document.createTextNode(index);         // Create a text node
+        node.appendChild(textnode);                              // Append the text to <li>
+        document.getElementById("even_number").appendChild(node);     // Append <li> to <ul> with id="myList"
+
+        }
     }
 }
 
 function for_odds(index,range){
     for(index;index<=range;index+=2){                   //for odd number
-        if(index%2 != 0)
+        if(index%2 != 0){
         console.log(index);
+         
+        var node = document.createElement("LI");                 // Create a <li> node
+        var textnode = document.createTextNode(index);         // Create a text node
+        node.appendChild(textnode);                              // Append the text to <li>
+        document.getElementById("odd_number").appendChild(node);     // Append <li> to <ul> with id="myList"
+        
+        }
     }
 }
 
@@ -64,9 +104,15 @@ function for_odds(index,range){
 //recurcive function start
 
 const recurcive_even =(index,range)=>{
-    if(index%2 == 0)
+    if(index%2 == 0){
     console.log(index);
 
+    var node = document.createElement("LI");                 // Create a <li> node
+    var textnode = document.createTextNode(index);         // Create a text node
+    node.appendChild(textnode);                              // Append the text to <li>
+    document.getElementById("even_number").appendChild(node);     // Append <li> to <ul> with id="myList"
+
+    }
     if(index<range){
         recurcive_even(++index,range)
     }
@@ -74,9 +120,15 @@ const recurcive_even =(index,range)=>{
 
 
 const recurcive_odd =(index,range)=>{
-    if(index%2 != 0)
+    if(index%2 != 0){
     console.log(index);
-
+     
+    var node = document.createElement("LI");                 // Create a <li> node
+    var textnode = document.createTextNode(index);         // Create a text node
+    node.appendChild(textnode);                              // Append the text to <li>
+    document.getElementById("odd_number").appendChild(node);     // Append <li> to <ul> with id="myList"
+    
+    }
     if(index<range){
         recurcive_odd(++index,range)
     }
